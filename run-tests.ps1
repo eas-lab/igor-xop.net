3f01b394ec9b3a0ad3bd2508fe65926020b90174
Gabriel Rosenhouse <gir@uchicago.edu>
2013-11-30 13:15:48 -0600

    run-tests script now agnostic to working directory

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,8 +1,10 @@
 $igor='"C:\Program Files (x86)\WaveMetrics\Igor Pro Folder\Igor.exe"'
 $curdir=pwd
-$test_file=join-path $curdir "xop-tests.ipf"
+$test_file=Join-Path $PSScriptRoot "xop-tests.ipf"
 $double_slashed=$test_file.Replace("\","\\")
 $ar='/i /x "Execute/P "LOADFILE ' + $double_slashed + '"; Execute/P "COMPILEPROCEDURES "; Execute/P "run_all_tests()""'
-"$igor $ar" | Out-File run-tests.cmd -Encoding ASCII
-cmd /c call run-tests.cmd
-rm run-tests.cmd
+
+$tempscript = Join-Path $PSScriptRoot "local-test-runner.cmd"
+"$igor $ar" | Out-File $tempscript -Encoding ASCII
+cmd /c call $tempscript
+rm $tempscript
