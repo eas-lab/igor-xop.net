ce23fa23a9cc69302306467e2add15b84f7e49b0
Gabriel Rosenhouse <gir@uchicago.edu>
2013-11-30 13:40:58 -0600

    Touch ups to run-tests.ps1

--- run-tests.ps1
+++ run-tests.ps1
@@ -1,5 +1,14 @@
+#REQUIRES -Version 3.0
+# 
+# Runs a simple test of the XOP functionality from inside Igor Pro
+# A shortcut to the build output (MyXOP.xop) needs to be in your Igor Extensions folder 
+#   C:\Users\<MyName>\Documents\WaveMetrics\Igor Pro 6 User Files\Igor Extensions
+#
+# To run this script, you may need to set the ExecutionPolicy on your machine to RemoteSigned
+#
+
+
 $igor='"C:\Program Files (x86)\WaveMetrics\Igor Pro Folder\Igor.exe"'
-$curdir=pwd
 $test_file=Join-Path $PSScriptRoot "xop-tests.ipf"
 $double_slashed=$test_file.Replace("\","\\")
 $ar='/i /x "Execute/P "LOADFILE ' + $double_slashed + '"; Execute/P "COMPILEPROCEDURES "; Execute/P "run_all_tests()""'
